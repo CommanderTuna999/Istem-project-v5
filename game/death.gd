2f66efb3ed689a2e7db6886cb42228bbc4981e29
Lucas Wan <lucas.wan1@education.nsw.gov.au>
2026-09-02 17:08:37 +1000

    disabled sound on weird weed ability
    sound script caused crashes i aint bothered to fix

---
+++ game/death.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Restart"):
+		get_tree().call_deferred("change_scene_to_file", "res://Scenes/Game.tscn")
+		get_tree().call_deferred("reload_current_scene")
